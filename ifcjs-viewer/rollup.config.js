a364dd1dde5adc16ba3e0c2c9e24b04c4fa4cc89
Alex Babinoff <alex.babinoff.cheat@gmail.com>
2025-07-02 12:59:11 +0300

    rollup plugins commonjs

--- ifcjs-viewer/rollup.config.js
+++ ifcjs-viewer/rollup.config.js
@@ -1,4 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default [{
   input: 'bimviewer.js',
@@ -9,7 +10,11 @@ export default [{
     },
   ],
   plugins: [
-    resolve(),
+    resolve({
+      browser: true,
+      preferBuiltins: false
+    }),
+    commonjs()
   ]
 }, 
 {
@@ -21,6 +26,10 @@ export default [{
     },
   ],
   plugins: [
-    resolve(),
+    resolve({
+      browser: true,
+      preferBuiltins: false
+    }),
+    commonjs()
   ]
 }];
